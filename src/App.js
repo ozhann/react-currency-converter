472a17c69f7c23ab4cb4edd581c14b9133121e6b
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-03-30 20:00:01 +0200

    basic interface

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import CurrencyRow from "./CurrencyRow";
+
+const BASE_URL = "https://api.exchangeratesapi.io/latest";
+
+function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  console.log(currencyOptions);
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data =>
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      );
+  }, []);
+
+  return (
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow currencyOptions={currencyOptions} />
+      <div className="equals">=</div>
+      <CurrencyRow currencyOptions={currencyOptions} />
+    </>
+  );
+}
+
+export default App;
